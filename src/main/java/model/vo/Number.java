b08adbade749598dc81936cd046b1db637be6871
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-11 17:12:18 +0900

    refactor : 정적팩토리와 생성자의 매개변수 타입 변경, getValue 메서드 삭제
    매개변수 타입 Integer -> String으로 변경
    
    필드를 반환하는 getValue 메서드 삭제하고 Number 객체를 받아서 값 차이를 반환하는 메서드로 대치

--- src/main/java/model/vo/Number.java
+++ src/main/java/model/vo/Number.java
@@ -4,23 +4,28 @@ public class Number {
     private final Integer value;
     private static final Integer MIN_VALUE = 0;
     private static final Integer MIX_VALUE = 24;
+    private static final String NUMBER_REGEX = "^[0-9]*$";
 
-    private Number(final Integer value) {
-        this.value = value;
+    private Number(final String value) {
+        this.value = Integer.parseInt(value);
     }
 
-    public static Number generate(final Integer value) {
+    public static Number generate(final String value) {
         validateNumber(value);
         return new Number(value);
     }
 
-    private static void validateNumber(final Integer value) {
-        if (value < MIN_VALUE || value > MIX_VALUE) {
+    private static void validateNumber(final String value) {
+        if(!value.matches(NUMBER_REGEX)) {
+            throw new IllegalArgumentException("좌표 값에 숫자가 아닌 입력 혹은 음수 입력이 있습니다.");
+        }
+        int number = Integer.parseInt(value);
+        if (number < MIN_VALUE || number > MIX_VALUE) {
             throw new IllegalArgumentException("점은 0~24 사이의 값만 가질 수 있습니다.");
         }
     }
 
-    public Integer getValue() {
-        return value;
+    public Integer getDifference(Number another) {
+        return value - another.value;
     }
 }
