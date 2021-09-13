3c5a8d305f6fb691a626e0fa1f0eea888e73579e
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-13 15:14:29 +0900

    feat : CoordinateController 구현

---
+++ src/main/java/controller/CoordinateController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import model.Line;
+import model.dto.InputDTO;
+import view.Display;
+
+import java.util.Scanner;
+
+public class CoordinateController {
+    private final Line line;
+    private final Display display;
+    private final Scanner scanner;
+    private final InputDTO inputDTO;
+    private boolean wrongInput = true;
+
+    private CoordinateController(Scanner scanner, Display display) {
+        this.scanner = scanner;
+        this.display = display;
+        inputDTO = new InputDTO();
+        line = Line.generate(inputLine());
+    }
+
+    public static CoordinateController generate(Scanner scanner, Display display) {
+        return new CoordinateController(scanner, display);
+    }
+
+    private String[] inputLine() {
+        String[] points = new String[2];
+        while (wrongInput) {
+            points = inputPoints();
+        }
+        return points;
+    }
+
+    private String[] inputPoints() {
+        String[] points = new String[2];
+        try {
+            points = inputDTO.splitTwoPoints(scanner.nextLine());
+            wrongInput = false;
+        } catch (IllegalArgumentException exception) {
+            display.printExceptionMessage(exception.getMessage());
+        }
+        return points;
+    }
+
+    public void run() {
+        display.printCoordinate(line.getPoints());
+        display.printLength(line.getLength());
+    }
+}
