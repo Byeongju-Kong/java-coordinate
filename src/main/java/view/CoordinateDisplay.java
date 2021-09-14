89bb4f969bbd8b51d62f11b3826fad24edfee710
Byeongju-Kong <bjuuuu98@gmail.com>
2021-09-14 15:24:14 +0900

    feat : CoordinateDisplay 구현

---
+++ src/main/java/view/CoordinateDisplay.java
@@ -0,0 +1,70 @@
+package view;
+
+import model.point.Point;
+
+import java.util.List;
+
+public class CoordinateDisplay implements Display {
+    private static final String DOUBLE_SPACE = "  ";
+    private static final int COORDINATE_BOUNDARY = 24;
+
+    public void printCoordinate(List<Point> points) {
+        String xAxis = "ㅡ";
+        int lengthForXAxis = 60;
+        for (int index = COORDINATE_BOUNDARY; index > 0; index--) {
+            printYAxisAndPoints(index, points);
+        }
+        System.out.println(DOUBLE_SPACE + "+  " + xAxis.repeat(lengthForXAxis));
+        System.out.println(printXAxis());
+    }
+
+    private void printYAxisAndPoints(int yIndex, List<Point> points) {
+        StringBuilder result = new StringBuilder();
+        result.append(generateYAxis(yIndex));
+        result.append(drawPoint(yIndex, points));
+        System.out.println(result);
+    }
+
+    private String drawPoint(int yIndex, List<Point> points) {
+        String tripleSpace = "   ";
+        String nothing = " ";
+        if (yIndex == points.get(0).getY()) {
+            return tripleSpace.repeat(points.get(0).getX()) + "*";
+        }
+        if (yIndex == points.get(1).getY()) {
+            return tripleSpace.repeat(points.get(1).getX()) + "*";
+        }
+        return nothing;
+    }
+
+    private String generateYAxis(int yIndex) {
+        String yAxis = "ㅣ";
+        StringBuilder result = new StringBuilder();
+        if (yIndex % 2 == 1) {
+            return DOUBLE_SPACE + yAxis;
+        }
+        if (yIndex < 10) {
+            result.append(" ");
+        }
+        return result.append(yIndex).append(yAxis).toString();
+    }
+
+    private String printXAxis() {
+        StringBuilder xAxis = new StringBuilder("   ");
+        for(int i = 0; i <= COORDINATE_BOUNDARY; i += 2) {
+            xAxis.append(i).append("     ");
+        }
+        return xAxis.toString();
+    }
+
+    public void printLength(double length) {
+        System.out.println("두 점 사이 거리는  " + length);
+    }
+
+    public void printExceptionMessage(String message) {
+        System.out.println(message + "다시 입력하세요.");
+    }
+}
+
+//ㅡ ㅡ
+//
